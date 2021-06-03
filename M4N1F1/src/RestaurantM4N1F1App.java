8ffc1cb50c5bb472798a6d8237a4d09b3a519a59
patri <patri@DESKTOP-SCNG0T2>
2021-06-03 17:49:27 +0200

    Initial commit

---
+++ M4N1F1/src/RestaurantM4N1F1App.java
@@ -0,0 +1,18 @@
+
+public class RestaurantM4N1F1App {
+
+	public static void main(String[] args) {
+		 
+		// Creem variables int per cada denominació de bitllets i pel preu total del menjar
+		
+		int bitllet500 = 0, bitllet200 = 0, bitllet100 = 0, bitllet50 = 0, bitllet20 = 0, bitllet10 = 0, bitllet5 = 0;
+		int preuMenjar = 0;
+		
+		// Creem un array String pels plats del menú i un array int pel preu de cada plat
+		
+		String plats[] = new String[10];
+		int preus[] = new int[10];
+					
+	}
+
+}
